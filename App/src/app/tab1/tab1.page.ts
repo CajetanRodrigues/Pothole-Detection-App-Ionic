323f96a404d2c23b59ce6b6aef86cbc246ae2f3d
Unknown <rigrod1999@gmail.com>
2019-03-21 13:55:51 +0530

    v 1.0
    Camera Upload feature done
    Created Login and Signup page
    Tested on real device

--- App/src/app/tab1/tab1.page.ts
+++ App/src/app/tab1/tab1.page.ts
@@ -1,8 +1,56 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  currentImage: any;
+
+  constructor(private camera: Camera) { }
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
+  }
+  // constructor(private cameraPreview: CameraPreview){
+    // const cameraPreviewOpts: CameraPreviewOptions = {
+    //   x: 0,
+    //   y: 0,
+    //   width: window.screen.width,
+    //   height: window.screen.height,
+    //   camera: 'rear',
+    //   tapPhoto: true,
+    //   previewDrag: true,
+    //   toBack: true,
+    //   alpha: 1
+    // }
+    
+    // // start camera
+    // this.cameraPreview.startCamera(cameraPreviewOpts).then(
+    //   (res) => {
+    //     console.log(res)
+    //   },
+    //   (err) => {
+    //     console.log(err)
+    //   });
+    
+  // }
+ 
+  
+    
+ 
+ 
+}
